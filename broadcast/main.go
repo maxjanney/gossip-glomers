23086a50b779579e9a5577e3a5bb45048f322d6c
maxjanney <majanney@me.com>
2023-09-02 23:54:13 -0400

    holy shit, this sometimes works

--- broadcast/main.go
+++ broadcast/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 	"log"
+	"sync"
+	"time"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 type Request struct {
@@ -12,90 +15,96 @@ type Request struct {
 }
 
 func main() {
+	var mu sync.Mutex
 	neighbors := make([]string, 0, 5)
 	state := make(map[int]struct{})
-	retry := make(chan Request)
 	node := maelstrom.NewNode()
 
-	go func() {
-		for {
-			req := <-retry
-			for {
-				body := map[string]any{"type": "gossip", "message": req.msg}
-				if err := node.Send(req.node, body); err == nil {
-					break
-				}
+	node.Handle("init", func(msg maelstrom.Message) error {
+		for _, n := range node.NodeIDs() {
+			if n == node.ID() {
+				continue
 			}
+			neighbors = append(neighbors, n)
 		}
-	}()
+		return node.Reply(msg, map[string]any{"type": "init_ok"})
+	})
 
 	node.Handle("broadcast", func(msg maelstrom.Message) error {
+		respBody := map[string]any{"type": "broadcast_ok"}
+		node.Reply(msg, respBody)
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
-		respBody := map[string]any{"type": "broadcast_ok"}
 		m := int(body["message"].(float64))
+		mu.Lock()
 		if _, ok := state[m]; ok {
-			return node.Reply(msg, respBody)
+			mu.Unlock()
+			return nil
 		}
 		state[m] = struct{}{}
-		for _, n := range neighbors {
-			gossipBody := map[string]any{"type": "gossip", "message": m}
-			if err := node.Send(n, gossipBody); err != nil {
-				retry <- Request{node: n, msg: m}
-				// return err
+		mu.Unlock()
+		unack := createUnack(neighbors, msg.Src)
+		for len(unack) > 0 {
+			for n := range unack {
+				rpcBody := map[string]any{"type": "broadcast", "message": m}
+				node.RPC(n, rpcBody, func(msg maelstrom.Message) error {
+					var body map[string]any
+					if err := json.Unmarshal(msg.Body, &body); err != nil {
+						return err
+					}
+					if body["type"] == "broadcast_ok" {
+						delete(unack, n)
+					}
+					return nil 
+				})
 			}
+			time.Sleep(time.Second)
 		}
-		return node.Reply(msg, respBody)
+		return nil 
 	})
 
 	node.Handle("read", func(msg maelstrom.Message) error {
+		mu.Lock()
 		messages := make([]int, 0, len(state))
 		for m := range state {
 			messages = append(messages, m)
 		}
+		mu.Unlock()
 		body := map[string]any{"type": "read_ok", "messages": messages}
 		return node.Reply(msg, body)
 	})
 
 	node.Handle("topology", func(msg maelstrom.Message) error {
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-		topology := body["topology"].(map[string]any)
-		nodes := topology[node.ID()].([]any)
-		for _, n := range nodes {
-			neighbors = append(neighbors, n.(string))
-		}
+		// var body map[string]any
+		// if err := json.Unmarshal(msg.Body, &body); err != nil {
+		// 	return err
+		// }
+		// topology := body["topology"].(map[string]any)
+		// nodes := topology[node.ID()].([]any)
+		// for _, n := range nodes {
+		// 	neighbors = append(neighbors, n.(string))
+		// }
 		return node.Reply(msg, map[string]any{"type": "topology_ok"})
 	})
 
-	node.Handle("gossip", func(msg maelstrom.Message) error {
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-		m := int(body["message"].(float64))
-		if _, ok := state[m]; ok {
-			return nil
-		}
-		state[m] = struct{}{}
-		for _, n := range neighbors {
-			if n == msg.Src {
-				continue
-			}
-			gossipBody := map[string]any{"type": "gossip", "message": m}
-			if err := node.Send(n, gossipBody); err != nil {
-				retry <- Request{node: n, msg: m}
-				// return err
-			}
-		}
-		return nil
+	node.Handle("broadcast_ok", func(msg maelstrom.Message) error {
+		return nil 
 	})
 
 	if err := node.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func createUnack(neighbors []string, src string) map[string]struct{} {
+	unack := make(map[string]struct{})
+	for _, n := range neighbors {
+		if n == src {
+			continue
+		}
+		unack[n] = struct{}{}
+	}
+	return unack
 }
